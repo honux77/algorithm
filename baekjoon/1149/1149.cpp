a19917d716d2d609bc7733c5c23cbf59fda04eab
DESKTOP-R3ML8O0\k5020 <honux77@gmail.com>
2018-08-18 00:39:50 +0900

    BOJ 1149 update
    clean code (just little bit)

--- baekjoon/1149/1149.cpp
+++ baekjoon/1149/1149.cpp
@@ -3,50 +3,38 @@
 
 using namespace std;
 
-enum Color { red = 0, green = 1, blue = 2 };
+enum Color {red, green, blue};
 int d[1001][3];
 int rgb[1001][3];
 
-//get minimum value from previous answer
-//Is there any way to improve this code? 
 int minValue(int idx, Color exclude) {
-	Color c1, c2;
-	if (exclude == red) {
-		c1 = green;
-		c2 = blue;
+	if (idx == 0) {
+		return 0;
 	}
-	else if (exclude == green) {
-		c1 = red;
-		c2 = blue;
+
+	int res = 98764321;
+
+	for (int c = red; c <= blue; c++) {
+		if (c != exclude) {
+			res = min(res, d[idx][c]);
+		}
 	}
-	else {
-		c1 = red;
-		c2 = green;
-	}
-	return min(d[idx][c1], d[idx][c2]);
+	return res;
 }
 
 int main() {
 	int n;
 	cin >> n;
+
 	for (int i = 1; i <= n; i++) {
-		cin >> rgb[i][red] >> rgb[i][green] >> rgb[i][blue];
-	}
-
-	d[1][red] = rgb[1][red];
-	d[1][green] = rgb[1][green];
-	d[1][blue] = rgb[1][blue];
-
-	for (int i = 2; i <= n; i++) {
+		cin >> rgb[i][red] >> rgb[i][green] >> rgb[i][blue];		
 		for (int j = red; j <= blue; j++) {
 			Color color = static_cast<Color>(j);
 			d[i][color] = minValue(i - 1, color) + rgb[i][color];
 		}
 	}
 
-	int ans = d[n][red];
-	ans = d[n][blue] < ans ? d[n][blue] : ans;
-	ans = d[n][green] < ans ? d[n][green] : ans;
+	int ans = min({d[n][red], d[n][blue], d[n][green]});
 	cout << ans << endl;
 	return 0;
 }
