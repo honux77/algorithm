29bd551d8095048d74623a0ddd7afe90ec9ae534
DESKTOP-R3ML8O0\k5020 <honux77@gmail.com>
2018-08-24 01:18:08 +0900

    백준 1914 하노이 개선
    cpp에서 big int 값을 표시할 수 있도록 수정

--- baekjoon/recursion/1914-hanoi.cpp
+++ baekjoon/recursion/1914-hanoi.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include <cstdio>
+#include <cmath>
 
 int cnt = 0;
 
@@ -7,12 +9,12 @@ using namespace std;
 
 void hanoi(int num, int src, int target) {
     if (num == 1) {        
-        printf("%d %d", src, target);
+        printf("%d %d\n", src, target);
         return;
     }
     int other = 6 - src - target;
     hanoi(num - 1, src, other);
-    printf("%d %d", src, target);
+    printf("%d %d\n", src, target);
     hanoi(num - 1, other, target);
 }
 
@@ -21,8 +23,15 @@ int main() {
     int n;
     bool print = false;
     cin >> n;
-    int ans = (int) pow(2, n) - 1;
-    cout << ans << endl;
+
+    //get 2^n - 1 (bigint)
+    string s = to_string(pow(2, n));
+    int idx = s.find('.');
+    s = s.substr(0, idx);
+    s[s.length() - 1] -= 1;
+    
+    cout << s << endl;
+    
     if (n <= 20) {
         hanoi(n, 1, 3);
     }    
