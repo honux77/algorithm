84a7ce8a3844d6ef801939232254b9effd2fba3c
Hoyoung Jung <honux77@gmail.com>
2018-09-15 00:31:53 +0900

    BOJ 16122 Fail (2)
    메모리 초과를 해결하기 위해 배열을 맵으로 바꿨으나
    시간 초과.

--- baekjoon/16122/16122-bin-op.cpp
+++ baekjoon/16122/16122-bin-op.cpp
@@ -2,42 +2,46 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 #define INF 998244353
 using ll = long long;
 
 using namespace std;
-vector <vector <ll> > d;
+vector<map<int, ll>> d;
 int n, m;
 
-//get new
-inline ll get(int r, int c){
-    return d[r][c + n];
+inline map<int, ll>::iterator get(int r, int c)
+{
+    return d[r].find(c);
 }
 
-inline void set(int r, int c, ll v) {
-    d[r][c + n] = v;
+inline void set(int r, int c, ll v)
+{
+    d[r][c] = v;
 }
 
-ll solve(int n, int m) {
-    //cout << n << "," << m << ": " << get(n, m) << endl;
-
-    if (get(n, m) == -1) {
+ll solve(int n, int m)
+{
+    if (get(n, m) == d[n].end())
+    {
         ll v = (solve(n - 1, -m) + solve(n - 1, -m - 1)) % INF;
         set(n, m, v);
     }
-    return get(n, m);
+    return get(n, m)->second;
 };
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     cin >> n >> m;
-    
-   d.resize(n + 1, vector<ll>(2 * n + 1, -1));
-    
-    for (int i = -n; i <= n; i++) {
+
+    d.resize(n + 1);
+
+    for (int i = -n; i <= n; i++)
+    {
         set(0, i, 0);
     }
     set(0, 0, 1);
