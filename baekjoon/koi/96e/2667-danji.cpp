89fb2cf037ad088b8e608950e6655123d29da5d6
Hoyoung Jung <honux77@gmail.com>
2018-09-28 16:19:37 +0900

    [BOJ] 2667 split apartment
    KOI 1996 초등 1번아파트 나누기
    내일 풀기 위해 기본 골격만 완성

---
+++ baekjoon/koi/96e/2667-danji.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+char a[25][25];
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		for (int j = 0; j < n; j++) {
+			a[i][j] = s[j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << a[i][j];
+		}
+		cout << "\n";
+	}
+	return 0;
+}
