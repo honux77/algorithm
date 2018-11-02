88606325aad87290755b529a56f31f7024222ce2
Hoyoung Jung <honux77@gmail.com>
2018-11-02 11:10:34 +0900

    [BOJ] 1149 rgb distance
    BOJ 1149 rgb distance
    
    그리 어렵지 않은 DP 문제
    난이도 2.5

--- baekjoon/dp/1149-RGBDistance.cpp
+++ baekjoon/dp/1149-RGBDistance.cpp
@@ -5,14 +5,14 @@ int d[1001][3];
 int main() {
 	int n;
 	cin >> n;
-	int rgb[3];
+	int r, g, b;
 	for (int i = 1; i <= n; i++) {
-		cin >> rgb[0] >> rgb[1] >> rgb[2];		
-		d[i][0] = min(d[i - 1][1], d[i - 1][2]) + rgb[0];
-        d[i][1] = min(d[i - 1][0], d[i - 1][2]) + rgb[1];
-        d[i][2] = min(d[i - 1][0], d[i - 1][1]) + rgb[2]; 		
+		cin >> r >> g >> b;		
+		d[i][0] = min(d[i - 1][1], d[i - 1][2]) + r;
+        d[i][1] = min(d[i - 1][0], d[i - 1][2]) + g;
+        d[i][2] = min(d[i - 1][0], d[i - 1][1]) + b; 		
 	}
-	int ans = min({d[n][0], d[n][1], d[n][2]});
+	int ans = min({d[n][0], d[n][1], d[n][2]});	
 	cout << ans << endl;
 	return 0;
 }
--- baekjoon/dp/1149-rgb.py
+++ baekjoon/dp/1149-rgb.py
@@ -1,9 +1,10 @@
 n = int(input())
-d = [[0, 0, 0]] * (n + 1)
+d = [[0,0,0]]
 for i in range(1, n + 1):
-    (r, g, b) = [int(i) for i in input().split()]
-    d[i][0] = r + min(d[i - 1][1], d[i - 1][2])
-    d[i][1] = g + min(d[i - 1][0], d[i - 1][2])
-    d[i][2] = b + min(d[i - 1][0], d[i - 1][1])
+    (r, g, b) = [int(i) for i in input().split()]    
+    dr = r + min(d[i - 1][1], d[i - 1][2])
+    dg = g + min(d[i - 1][0], d[i - 1][2])
+    db = b + min(d[i - 1][0], d[i - 1][1])
+    d.append([dr, dg, db])    
 
 print(min(d[n]))
