246df98aa60e31fee863dddad749df4ae52f5314
Hoyoung Jung <honux77@gmail.com>
2019-04-06 21:01:40 +0900

    Solve code jam #1
    간단하게 잘 풀었음.
    햄버거 먹다가 생각님

--- codejam/20190406/one.py
+++ codejam/20190406/one.py
@@ -1,28 +1,24 @@
 n = int(input())
 
-def check4(num):
-    i = 1
-    while num != 0:
-        d = num % 10
-        if d == 4:
-            return i
-        num //= 10
-        i *= 10
-    return -1
+def makeNum(arr):
+    while arr[0] == 0:
+        arr = arr[1:]
+    return "".join([str(i) for i in arr])
+
 
 for p in range(1, n + 1):
-    r = int(input())
-    x = 1
-    while x < r:    
-        step = check4(x)
-        if step > 0:
-            x += step
-            continue        
-        y = r - x
-        if check4(y) > 0:
-            x += 1
-            continue            
+    r = input()
+    n1 = []
+    n2 = []
+    for c in r:
+        x = int(c)
+        if x == 4:
+           n1.append(2)
+           n2.append(2)
         else:
-            print("Case #{}: {} {}".format(p, x, y))
-            break
-            
+            n1.append(x)
+            n2.append(0)  
+    
+    num1 = makeNum(n1)
+    num2 = makeNum(n2)
+    print("Case #{}: {} {}".format(p, num1, num2))           
