104354f1d1c3511c6730f59a19dc103ee4621b3e
Hoyoung Jung <honux77@gmail.com>
2019-04-07 08:38:14 +0900

    Still RE
    여전히 안 풀림
    난 최선을 다했다...

--- codejam/20190406/three.py
+++ codejam/20190406/three.py
@@ -1,43 +1,38 @@
 t = int(input())
 
+import math
+
 def solve(t):
-    nmax, size = [int(i) for i in input().split()]      
+    _, l = [int(i) for i in input().split()]      
     nums = [int(i) for i in input().split()]  
-    prime = {}
+    ptable = {}
     rnums = []
 
-    for i in range(size):
+    for i in range(l):
         if i == 0:
-            for j in range(3, nmax + 1, 2):
-                if nums[i] % j == 0:            
-                    rnums.append(j)
-                    rnums.append(nums[i]//j)                    
-                    prime[j] = prime[nums[i] // j] = True
-                    break
-        elif i == 1:
-            if nums[i] % rnums[0] == 0:
-                rnums[0], rnums[1] = rnums[1], rnums[0]
-            
-            p = nums[i] // rnums[i]
-            prime[p] = True
-            rnums.append(p)
+            second = math.gcd(nums[i], nums[i + 1])
+            first = nums[0] // second
+            rnums.append(first)
+            rnums.append(second)
+            ptable[first] = '@'
+            ptable[second] = '#'
         else:
-            p = nums[i] // rnums[i]
-            prime[p] = True
-            rnums.append(p)
+           prime = nums[i] // rnums[i]
+           rnums.append(prime)
+           if prime not in ptable:
+               ptable[prime] = '*'
 
     cha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    sp = sorted(prime.keys())    
-    for i in range(len(sp)):
-        prime[sp[i]] = cha[i]
-    
-    ans = []
-    for p in rnums:
-        ans.append(prime[p])
+    sp = sorted(ptable.keys())    
 
-    print("Case #{}: {}".format(t, "".join(ans)))
+    for i in range(len(sp)):
+        ptable[sp[i]] = cha[i]
+    
+    ans = ""
+    for p in rnums:
+        ans +=ptable[p]
+
+    print("Case #{}: {}".format(t, ans))
 
 for i in range(1, t + 1):
     solve(i)
-
-
