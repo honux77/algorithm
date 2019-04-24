664f7d65a1703dbc5978f4082f1200a367869d7b
Hoyoung Jung <honux77@gmail.com>
2019-04-24 15:47:15 +0900

    BOJ 2747 fibonacci
    피보나치 수열 문제
    DP로 풀린다.
    난이도 1점
    
    스펠링이 맞는지 모르겠네..

---
+++ baekjoon/easy-fibbonacci/2747-fibo.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	vector<int> d(n + 1);
+	d[0] = 0;
+	d[1] = 1;
+	
+	for (int i = 2; i <= n; i++) {
+		d[i] = d[i - 1] + d[i - 2];
+	}
+
+	cout << d[n] << endl;	
+	return 0;
+}
