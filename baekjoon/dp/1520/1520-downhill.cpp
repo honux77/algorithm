71fc18d79c3dc3110d15341e1f51b8b4e51cc241
Hoyoung Jung <honux77@gmail.com>
2019-05-08 01:55:26 +0900

    Solve BOJ 1520
    Yeah!
    계산하는 순서를 바로잡아서 해결!

--- baekjoon/dp/1520/1520-downhill.cpp
+++ baekjoon/dp/1520/1520-downhill.cpp
@@ -2,37 +2,38 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <queue>
+#include <tuple>
 
 using namespace std;
+int d[501][501];
+int c[501][501];
+using ituple = tuple<int, int, int>;
+
 
 int main() {
 	int m, n;
 	scanf("%d %d", &m, &n);
-	vector<vector<int>> c(m), d(m);
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
+	vector<ituple> rank;
+
+	for (int i = 1; i <= m; i++) {
+		for (int j = 1; j <= n; j++) {
 			int u;
 			scanf("%d", &u);
-			c[i].push_back(u);
-			d[i].push_back(0);
+			c[i][j] = u;
+			rank.push_back(make_tuple(u, i, j));
 		}
 	}
-	d[0][0] = 1;
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			if (i > 0 && c[i][j] < c[i - 1][j]) d[i][j] += d[i - 1][j];
-			if (i < m - 1 && c[i][j] < c[i + 1][j]) d[i][j] += d[i + 1][j];
-			if (j > 0 && c[i][j] < c[i][j - 1]) d[i][j] += d[i][j - 1];
-			if (j < n - 1 && c[i][j] < c[i][j + 1]) d[i][j] += d[i][j + 1];
-		}
-	}
-	printf("%d\n", d[m-1][n-1]);
 
-	for (auto v : d) {
-		for (auto i : v) {
-			printf("%d", i);
-		}
-		printf("\n");
+	sort(rank.begin(), rank.end());
+	d[1][1] = 1;
+	for (int idx = rank.size() - 1; idx >= 0; idx--) {
+		int r = get<0>(rank[idx]);
+		int i = get<1>(rank[idx]);
+		int j = get<2>(rank[idx]);
+		if (i > 1 && c[i][j] < c[i - 1][j]) d[i][j] += d[i - 1][j];
+		if (i < m && c[i][j] < c[i + 1][j]) d[i][j] += d[i + 1][j];
+		if (j > 1 && c[i][j] < c[i][j - 1]) d[i][j] += d[i][j - 1];
+		if (j < n && c[i][j] < c[i][j + 1]) d[i][j] += d[i][j + 1];
 	}
+	printf("%d\n", d[m][n]);
 }
