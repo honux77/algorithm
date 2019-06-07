5d338c0a1fdfcd03173cdf77161500188fa95e47
호눅스 <honux77@gmail.com>
2019-06-07 15:30:23 +0000

    Solve BOJ 1436
    먼저 쉽게 짜고 내가 원하던 방법으로 푼 풀이와의 차이를 비교해서 오류를 수정함

--- baekjoon/easy-brute-force/1436-movie-ok.cpp
+++ baekjoon/easy-brute-force/1436-movie-ok.cpp
@@ -1,38 +1,28 @@
 #include <cstdio>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 bool check(int n) {
-	int count = 0;
-	int c = 0;
-	int p = 0;
-	while (n != 0) {
-		p = c;
-		c = n % 10;
-		n /= 10;
-		if (c == 6) { 
-			if (p!= 6) count = 1;
-			else count++;
-			if (count >= 3) return true;
-		}
-	}
-	return false;
+	string s = to_string(n);
+	return (s.find("666") != string::npos);
 }
 
 int main() {
 	int n;
 	cin >> n;
 	int count = 0;
+	int i = 1;
 	for(int i = 1;; i++) {
 		if (check(i)) {
-			cout << i << endl;
 			count++;
 			if (count == n) {
 				cout << i << endl;
 				return 0;
 			}
 		}
+	i++;
 	}
 	return 0;
 }
