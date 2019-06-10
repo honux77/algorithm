93023abdb786c60456756baaca2600c4386f63bc
호눅스 <honux77@gmail.com>
2019-06-10 13:28:35 +0000

    Solve BOJ 10953 A+B 6
    very easy

--- baekjoon/easy-math/10953-ab.cpp
+++ baekjoon/easy-math/10953-ab.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <cstdio>
 
 int main() {
+	int n;
+	scanf("%d", &n);
+	char buf[4];
+	while (n--) {
+		scanf("%s", buf);
+		int n1 = buf[0] - '0';
+		int n2 = buf[2] - '0';
+		printf("%d\n", n1 + n2);
+ 	}
 	return 0;
 }
