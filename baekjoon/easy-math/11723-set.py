609e8002f11b907438701d7b6c1a2883dd7e2465
Hoyoung Jung <honux77@gmail.com>
2020-06-23 14:58:36 +0900

    BOJ 11723 set - fix wrong answer

--- baekjoon/easy-math/11723-set.py
+++ baekjoon/easy-math/11723-set.py
@@ -1,27 +1,50 @@
-n = int(input())
-
-s = set()
 import sys
+
+# set
+s = [0] * 20
+
+def all():
+    global s
+    for i in range(0, 20):
+        s[i] = 1
+
+def empty():
+    global s
+    for i in range(0, 20):
+        s[i] = 0
+
+def add(n):
+    s[n - 1] = 1
+
+def remove(n):
+    s[n - 1] = 0
+
+def check(n):
+    print(int(s[n - 1] == 1))
+
+n = int(input())
 
 for _ in range(n):
     l = sys.stdin.readline().rstrip()
+    # print(l, s)
     if 'all' in l:
-        s = set(map(str,(range(1,21))));
+        all()
         continue
     elif 'empty' in l:
-        s = set()
+        empty()
         continue
     else:
         cmd, num = l.split()
+        num = int(num)
 
     if cmd == 'add':
-        s.add(num)
-    elif cmd == 'remove' and num in s:
-        s.remove(num)
+        add(num)
+    elif cmd == 'remove':
+        remove(num)
     elif cmd == 'check':
-        print(int(num in s))
+        check(num)
     elif cmd == 'toggle':
-        if num in s:
-            s.remove(num)
+        if s[num - 1] == 1:
+            remove(num)
         else:
-            s.add(num)
+            add(num)
