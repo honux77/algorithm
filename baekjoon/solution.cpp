23acbb1490f91a428fc0102b48d65f1230d321d7
Hoyoung Jung <honux77@gmail.com>
2020-10-08 09:44:55 +0900

    BOJ 10816 again

--- baekjoon/solution.cpp
+++ baekjoon/solution.cpp
@@ -1,15 +1,16 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-using i64 = long long int;
+using ull = long long int;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    
     int n;
     cin >> n;
     cout << n;
