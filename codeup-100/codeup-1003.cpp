89dc8aa897e3a5b99e7e126d91c518259a5dd03b
Hoyoung Jung <honux77@gmail.com>
2020-12-29 12:28:10 +0900

    Codeup 100 - 1

---
+++ codeup-100/codeup-1003.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+using ull = long long int;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "Hello\nWorld" << endl;    
+    return 0;
+}
---
+++ codeup-100/codeup-1007.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+using ull = long long int;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "\"C:\\Download\\hello.cpp\"" << endl;    
+    return 0;
+}
