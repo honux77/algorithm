e347278f14b57bea9f9977d358eb42063dc1d900
Hoyoung Jung <honux77@gmail.com>
2022-01-07 00:28:20 +0900

    Study PS

--- codeforces/practice/339b.cpp
+++ codeforces/practice/339b.cpp
@@ -27,36 +27,25 @@ using i64 = long long int;
 using ii = pair<int, int>;
 using ii64 = pair<i64, i64>;
 
-bool myless(ii a, ii b) {
-    return a.first < b.first;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int s, n;
-    cin >> s >> n;
-
-    vector<ii> m(n);
-    for (int i = 0; i < n; i++) {
-        int a, b;
-        cin >> a  >> b;
-        m[i] = {a, b};
+    i64 n, m;
+    cin >> n >> m;
+    i64 ans = 0;
+    i64 s = 1;
+    for (int i = 0; i < m; i++) {
+        i64 t;
+        cin >> t;
+        t = t % n;
+        if (s > t) {
+            t += n;
+        }       
+        ans += t - s;
+        s = t % n;        
     }
-
-    sort(m.begin(), m.end(), myless);
-    
-    for (auto &v : m) {
-        if (s <= v.first) {
-            cout << "NO\n";
-            return 0;            
-        } else {
-            s += v.second;
-        }
-    }
-
-    cout << "YES\n";
+    cout << ans << endl;
     return 0;
 }
