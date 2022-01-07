309c472986529c029c7aa16ea8ae24ea7abe768e
Hoyoung Jung <honux77@gmail.com>
2022-01-07 23:20:21 +0900

    Solve codeforce problems

---
+++ codeforces/practice/466a.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <map>
+#include <set>
+#include <tuple>
+#include <random>
+#include <functional>
+
+#define all(x) (x).begin(), (x).end()
+#define xx first
+#define yy second
+
+using namespace std;
+
+template<typename T, typename Pr = less<T>>
+using pq = priority_queue<T, vector<T>, Pr>;
+using i64 = long long int;
+using ii = pair<int, int>;
+using ii64 = pair<i64, i64>;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+
+    int t;    
+    t = n * a;    
+    t = min(t, (n / m) * b + (n % m) * a);    
+    t = min(t, ((int) ceil((double)n / m)) * b);       
+    cout << t << endl;
+    return 0;
+}
