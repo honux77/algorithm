3a639f0048300c64127144050816d51bb32905fb
Hoyoung Jung <honux77@gmail.com>
2022-08-18 22:20:14 +0900

    BOJ

---
+++ baekjoon/very-easy/5999.py
@@ -0,0 +1,6 @@
+a = input()
+b = input()
+if b in a: 
+    print("go")
+else:
+    print("no")
---
+++ baekjoon/very-easy/password.py
@@ -0,0 +1,7 @@
+n = int(input())
+for _ in range(n):
+    s = input()
+    if 6 <= len(s) <= 9:
+        print("yes")
+    else:
+        print("no")
