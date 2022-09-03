dd5b390b6610b9a8aeeddfc3dc95ce772e860c27
Hoyoung Jung <honux77@gmail.com>
2022-09-03 20:12:08 +0900

    Update script

--- prompt412/start.sh
+++ prompt412/start.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-cp -a template $1
+
+if [ $# -ne  1 ]; then
+  echo "Usage: $0 number_of_round"
+  exit 1
+fi
+
+round="round-$1"
+
+if [ -d $round ]; then
+  echo "$round already exists"
+  exit 1
+fi
+
+cp -a template $round
+echo "Done to create $round"
