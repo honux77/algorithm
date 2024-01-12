a9b61ff9056e4b402e30d47fa3aef28d6cbbe456
Hoyoung Jung <honux77@gmail.com>
2024-01-13 01:44:00 +0900

    Codeforce 1919a Wallet Exchange (Math 800)

---
+++ codeforces/practice-2024/1919a.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <map>
+#include <set>
+#include <tuple>
+#include <random>
+#include <functional>
+
+#define all(x) (x).begin(), (x).end()
+#define xx first
+#define yy second
+
+using namespace std;
+
+template<typename T, typename Pr = less<T>>
+using pq = priority_queue<T, vector<T>, Pr>;
+using i64 = long long int;
+using ii = pair<int, int>;
+using ii64 = pair<i64, i64>;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n = 0;
+        int a, b;
+        cin >> a >> b; 
+        int t = a + b;
+        if (t % 2 == 0) {
+            cout << "Bob\n";            
+        } else {
+            cout << "Alice\n";
+        }
+    }
+    return 0;
+}
